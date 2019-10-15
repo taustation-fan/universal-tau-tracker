4b18ccbcd4a5bd2056bc8d6afb54f6159b4174ff
tauhead <firefu@tauhead.com>
2019-10-15 11:10:21 +0000

    add @DownloadURL so auto-update works

--- tau-tracker.user.js
+++ tau-tracker.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Tau Station Universal Tracker
-// @version      0.7
+// @version      0.8
 // @author       Moritz Lenz <moritz.lenz@gmail.com>
 // @description  General data collection script for Tau Station. Please get an access token from moritz and add it in your preferences page.
 // @match        https://alpha.taustation.space/
 // @match        https://alpha.taustation.space/*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @require      https://rawgit.com/taustation-fan/userscripts/master/userscript-preferences.js
+// @downloadURL  https://github.com/taustation-fan/universal-tau-tracker/raw/master/tau-tracker.user.js
 // @grant        none
 // ==/UserScript==
 
