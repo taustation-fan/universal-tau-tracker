a969201e9fe994064db8d835210ce5a4dc018a33
Moritz Lenz <moritz.lenz@gmail.com>
2019-10-17 22:39:27 +0200

    Basic navigation

--- utt/app.py
+++ utt/app.py
@@ -1,7 +1,9 @@
 import os
 from utt.model import db
 from flask_cors import CORS
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
 
 def make_app():
     app = Flask(__name__)
@@ -17,3 +19,7 @@ app = make_app()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
