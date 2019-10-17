8d56f8923bd4a4b878c9e5573d7fdc8200496841
Moritz Lenz <moritz.lenz@gmail.com>
2019-10-17 18:29:48 +0200

    Fuel price overview

--- utt/fuel.py
+++ utt/fuel.py
@@ -71,3 +71,8 @@ def render_fuel_add_response(current_station):
             'age': gct_duration(start_dt - stat.last_reading),
         })
     return str(render_template('fuel_short_table.html', rows=rows))
+
+@app.route('/fuel')
+def fuel_list():
+    stats = FPS.query.order_by(FPS.system_name, FPS.station_level, FPS.station_name).all()
+    return render_template('fuel_list.html', rows=stats)
