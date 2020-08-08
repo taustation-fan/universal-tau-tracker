47431950c4cf8c38d130a44e3119c5ef4ef20136
Moritz Lenz <moritz.lenz@gmail.com>
2020-08-08 18:47:43 +0200

    Add missing file

---
+++ utt/ship.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timezone
+
+from sqlalchemy.orm.exc import NoResultFound
+from flask import request, jsonify
+
+from utt.app import app
+from utt.model import db, \
+    autovivify, \
+    get_station, \
+    Token, \
+    Ship, \
+    ShipClass, \
+    ShipSighting 
+
+def now():
+    return datetime.now(timezone.utc)
+
+@app.route('/v1/ship/add', methods=['POST'])
+def ship_add():
+    payload = request.get_json(force=True)
+    token = Token.verify(payload['token'])
+    station = get_station(payload['system'], payload['station'])
+    count = 0
+    for ship_sighting in payload['ships']:
+        ship_class = autovivify(ShipClass, {'name': ship_sighting['class']})
+        ship = autovivify(Ship, {
+            'registration': ship_sighting['registration'],
+            'ship_class': ship_class,
+            'name': ship_sighting['name'],
+            'captain': ship_sighting['captain'],
+        })
+        db.session.add(ShipSighting(
+            ship=ship,
+            station=station,
+            token=token,
+        ))
+        count += 1
+    db.session.commit()
+    return jsonify({'success': True, 'num_recorded': count})
