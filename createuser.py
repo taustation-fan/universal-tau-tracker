94d55f489054fda1bd65682cd8a50a140306ae71
Moritz Lenz <moritz.lenz@gmail.com>
2021-01-04 23:45:33 +0100

    Item comments (no edit interface yet)

--- createuser.py
+++ createuser.py
@@ -4,14 +4,14 @@ import sys
 
 from utt.model import db, Character, Token
 from utt import app
+from sqlalchemy.exc import IntegrityError
 
 name = sys.argv[1]
 
 def random_token():
     return ''.join(random.choices(string.ascii_letters, k=40))
 
-with app.app_context():
-    db.create_all()
+def insert(name):
     character = Character(name=name)
     db.session.add(character)
 
@@ -22,5 +22,15 @@ with app.app_context():
     token = Token(character=character, token=token_str)
     db.session.add(token)
     db.session.commit()
-
     print('{} - {}'.format(name, token_str))
+
+with app.app_context():
+    db.create_all()
+
+    character = Character.query.filter_by(name=name).first()
+    if character:
+        print('Character {} already exists, with these tokens:'.format(name))
+        for token in character.tokens:
+            print('   {}'.format(token.token))
+    else:
+        insert(name)
