57d60d8a2cd6c40d805257129191cff17293d211
Moritz Lenz <moritz.lenz@gmail.com>
2021-01-05 14:29:40 +0100

    Fix for today() function

--- utt/util.py
+++ utt/util.py
@@ -1,10 +1,12 @@
 import pytz
 from datetime import datetime, date
 
+tau_tz = pytz.timezone('Europe/Paris')
+
 def today_datetime():
-    n = datetime.now(pytz.timezone('Europe/Paris'))
+    n = datetime.now(tau_tz)
     return n.replace(hour=0, minute=0, second=0, microsecond=0).astimezone(pytz.UTC)
 
 def today():
-    dt = today_datetime()
+    dt = datetime.now(tau_tz)
     return date(dt.year, dt.month, dt.day)
